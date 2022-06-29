80c2542228ac1b5b34fa5256e63b0365d70bae5e
Selim Fazylov <selim.fazylov1@gmail.com>
2022-06-30 02:31:46 +0300

    added updateavatar, upload

--- controllers/auth/index.js
+++ controllers/auth/index.js
@@ -3,11 +3,13 @@ const login = require("./login");
 const getCurrent = require("./getCurrent");
 const logout = require("./logout");
 const changeSubscript = require("./subscription");
+const updateAvatar = require("./updateAvatar");
 
 module.exports = {
     register,
     login,
     getCurrent,
     logout,
-    changeSubscript
+    changeSubscript,
+    updateAvatar
 }

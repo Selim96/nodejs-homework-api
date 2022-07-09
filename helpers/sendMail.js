abab6c5c1f55f055ffc54be7a3344aefa492c89b
Selim Fazylov <selim.fazylov1@gmail.com>
2022-07-09 18:43:55 +0300

    changed sendmail

--- helpers/sendMail.js
+++ helpers/sendMail.js
@@ -1,42 +1,24 @@
-const nodemailer = require("nodemailer");
-require("dotenv").config();
+// const nodemailer = require("nodemailer");
+// require("dotenv").config();
 
-const { META_PASSOWRD } = process.env;
+// const { META_PASSOWRD } = process.env;
 
-const nodemailerConfig = {
-    host: "smtp.meta.ua",
-    port: 465,
-    secure: true,
-    auth: {
-        user: "selim94@meta.ua",
-        pass: META_PASSOWRD
-    }
-};
+// const nodemailerConfig = {
+//     host: "smtp.meta.ua",
+//     port: 465,
+//     secure: true,
+//     auth: {
+//         user: "selim94@meta.ua",
+//         pass: META_PASSOWRD
+//     }
+// };
 
-const transporter = nodemailer.createTransport(nodemailerConfig);
-
-const sendMail = async (data) => {
-    try {
-        const mail = { ...data, from: "selim94@meta.ua" }
-        await transporter.sendMail(mail);
-        return true;
-    } catch (error) {
-        throw error;
-    }
-};
-
-module.exports = sendMail;
-
-// ===================================================
-// const sgMail = require("@sendgrid/mail");
-// const { SENDGRID_API_KEY } = process.env;
-
-// sgMail.setApiKey(SENDGRID_API_KEY);
+// const transporter = nodemailer.createTransport(nodemailerConfig);
 
 // const sendMail = async (data) => {
 //     try {
-//         const mail = { ...data, from: "selim.fazylov1@gmail.com" };
-//         await sgMail.send(mail);
+//         const mail = { ...data, from: "selim94@meta.ua" }
+//         await transporter.sendMail(mail);
 //         return true;
 //     } catch (error) {
 //         throw error;
@@ -44,3 +26,23 @@ module.exports = sendMail;
 // };
 
 // module.exports = sendMail;
+
+// ===================================================
+const sgMail = require("@sendgrid/mail");
+require("dotenv").config();
+const { SENDGRID_API_KEY } = process.env;
+
+sgMail.setApiKey(SENDGRID_API_KEY);
+
+const sendMail = async (data) => {
+    try {
+        const mail = { ...data, from: "selim.fazylov1@gmail.com" };
+        await sgMail.send(mail);
+        return true;
+    } catch (error) {
+        console.log(error.message);
+        // throw error;
+    }
+};
+
+module.exports = sendMail;
